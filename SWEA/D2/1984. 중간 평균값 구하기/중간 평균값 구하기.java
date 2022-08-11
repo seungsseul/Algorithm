8ef6f031f5e201b3c3a0a0ebe32d894d797c3c1c
seungwan97 <101645602+seungwan97@users.noreply.github.com>
2022-08-11 09:27:48 +0900

    [D2] Title: 중간 평균값 구하기, Time: 136 ms, Memory: 20,852 KB -BaekjoonHub

--- SWEA/D2/1984. 중간 평균값 구하기/중간 평균값 구하기.java
+++ SWEA/D2/1984. 중간 평균값 구하기/중간 평균값 구하기.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution {
@@ -10,20 +9,34 @@ public class Solution {
 			for(int j=0;j<10;j++) {
 				arr[j] = sc.nextInt();
 			}
-			for(int k=0; k<arr.length-1;k++) {
-				for(int j=0; j<arr.length-1;j++) {
-					if(arr[j]>=arr[j+1]) {
-						int temp = arr[j+1];
-						arr[j+1]=arr[j];
-						arr[j]=temp;
-					}
+			//1.가장 큰 값 알아내기
+			int max=-1;
+			for(int j=0;j<arr.length;j++) {
+				if(max<arr[j]) {
+					max = arr[j];
 				}
 			}
+			//2.세기
+			int[] cnt_arr = new int[max+1];
+			for(int j=0;j<arr.length;j++) {
+				cnt_arr[arr[j]]++;
+			}
+			//3.누적합 구하기
+			for(int j=1;j<cnt_arr.length;j++) {
+				cnt_arr[j]=cnt_arr[j-1]+cnt_arr[j];
+			}
+			//4.정렬해서 넣기
+			int[] sort_arr = new int[arr.length];
+			for(int j=arr.length-1;j>=0;j--) {
+				cnt_arr[arr[j]]=cnt_arr[arr[j]]-1;
+				sort_arr[cnt_arr[arr[j]]]=arr[j];
+			}
 			int sum=0;
-			for(int k=1; k<arr.length-1;k++) {
-				sum+=arr[k];
+			for(int j=1;j<sort_arr.length-1;j++) {
+				sum+=sort_arr[j];
 			}
 			System.out.println("#"+i+" "+Math.round((float)sum/(arr.length-2)));
 		}
+		sc.close();
 	}
 }
