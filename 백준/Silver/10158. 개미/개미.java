5d182d3a80248186940c7628ee8d6b482433938c
seungwan97 <101645602+seungwan97@users.noreply.github.com>
2022-08-18 10:23:54 +0900

    [Silver IV] Title: 개미, Time: 72 ms, Memory: 11568 KB -BaekjoonHub

--- 백준/Silver/10158. 개미/개미.java
+++ 백준/Silver/10158. 개미/개미.java
@@ -4,25 +4,36 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+	static StringTokenizer st;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		st = new StringTokenizer(br.readLine());
 		int w = Integer.parseInt(st.nextToken());
 		int h = Integer.parseInt(st.nextToken());
-		StringTokenizer st2 = new StringTokenizer(br.readLine());
-		int p = Integer.parseInt(st2.nextToken());
-		int q = Integer.parseInt(st2.nextToken());
-		long t = Integer.parseInt(br.readLine());
-		long sR = (t + p) / w;
-		long rR = (t + p) % w;
-		long sC = (t + q) / h;
-		long rC = (t + q) % h;		
-		long aR = ( sR % 2 == 0 ) ? rR : w - rR;
-		long aC = ( sC % 2 == 0 ) ? rC : h - rC;	
+		st = new StringTokenizer(br.readLine());
+		int p = Integer.parseInt(st.nextToken());
+		int q = Integer.parseInt(st.nextToken());
+		int t = Integer.parseInt(br.readLine());
+		int tmp = t;
+		int movex = 1;
+		int movey = 1;
+		if(((p+t)/w)%2==0) {
+			p=(p+t)%w;
+		}
+		else {
+			p=w-(p+t)%w;
+		}
+		if(((q+t)/h)%2==0) {
+			q=(q+t)%h;
+		}
+		else {
+			q=h-(q+t)%h;
+		}
+		
 		StringBuilder sb = new StringBuilder();
-		sb.append(aR);
+		sb.append(p);
 		sb.append(" ");
-		sb.append(aC);
+		sb.append(q);
 		System.out.println(sb);
 		br.close();
 	}
