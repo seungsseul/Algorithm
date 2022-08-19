30f6ba9516322654bbb8f5caa87fc8c7e65175f4
seungwan97 <101645602+seungwan97@users.noreply.github.com>
2022-08-19 23:12:23 +0900

    [D1] Title: 자릿수 더하기, Time: 138 ms, Memory: 56,672 KB -BaekjoonHub

--- SWEA/D1/2058. 자릿수 더하기/자릿수 더하기.py
+++ SWEA/D1/2058. 자릿수 더하기/자릿수 더하기.py
@@ -1,5 +1,13 @@
-N=input()
-List=[]
-for i in N:
-    List.append(int(i))
-print(sum(List))
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int one = N%10;
+		int two = N%100/10;
+		int three = N%1000/100;
+		int four = N%10000/1000;
+		System.out.println(one+two+three+four);
+	}
+}
