ab9a8c1681bad402feabd87d81c66a3df5d23206
seungwan97 <101645602+seungwan97@users.noreply.github.com>
2022-09-16 22:14:21 +0900

    [Silver V] Title: 소수 찾기, Time: 116 ms, Memory: 12956 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.java
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.java
@@ -8,12 +8,15 @@ public class Main {
 		Loop1:
 		for(int t=1;t<=N;t++) {
 			int sosu=sc.nextInt();
+			//1은 소수가 아님
 			if(sosu!=1) {
+				//2부터 입력값까지 for문 돌려서 i가 소수가 아닌데 입력값%i가 0이면 카운트 세지않음
 				for(int i=2;i<=sosu;i++) {
 					if(i!=sosu && sosu%i==0) {
 						continue Loop1;
 					}
 				}
+				//continue Loop1에 안걸렸으면 소수이므로 카운트
 				cnt++;
 			}
 		}
