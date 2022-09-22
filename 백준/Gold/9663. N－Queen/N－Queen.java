225263677f761dd6e36df6efe7e769dec0142e6e
seungwan97 <101645602+seungwan97@users.noreply.github.com>
2022-09-22 16:13:22 +0900

    [Gold IV] Title: N-Queen, Time: 8644 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+
+public class Main {
+	static int N; 
+	static int[][] map;
+	static int ans;
+	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        ans = 0;
+        map = new int[N][N];
+
+        putQueen(0);
+        System.out.println(ans);
+
+	}
+
+	private static void putQueen(int idx) {
+		
+		if(idx == N){
+            ans++;
+            return;
+        }
+
+        for(int c = 0; c < N; c++){
+        	
+            if(isPutable(idx,c)) {
+            	
+                map[idx][c] = 1;
+                putQueen(idx+1);
+                map[idx][c] = 0;
+            }
+        }
+	}
+
+	private static boolean isPutable(int idx, int c) {
+		int left = c;
+        int right = c;
+         
+        for(int i = idx-1; i>=0; i--){
+            left--;
+            right++;
+             
+            if(map[i][c] == 1) { 
+            	return false;
+            }
+            if(left>=0 &&map[i][left] == 1) {
+            	return false; 
+            }
+            if(right <N&&map[i][right] == 1) {
+            	return false; 
+            }
+        }
+         
+        return true;
+	}
+}
