d2355cecc532314ec2778c58ea411172042e73f0
seungwan97 <101645602+seungwan97@users.noreply.github.com>
2022-10-06 14:03:57 +0900

    [D3] Title: 햄버거 다이어트, Time: 1,028 ms, Memory: 32,936 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,43 +1,49 @@
 import java.util.Scanner;
 
 public class Solution {
-	static int N, L, Ti, Ki, cal_sum, score_sum, max;
+	static int N, L, Ti, Ki , cal_sum, score_sum, max;
 	static int[] score_arr;
 	static int[] cal_arr;
 	static boolean[] check;
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
-		for (int t = 1; t <= T; t++) {
-			N = sc.nextInt(); // 재료의 수
-			L = sc.nextInt(); // 제한칼로리
-			score_arr = new int[N + 1];
-			cal_arr = new int[N + 1];
-			for (int r = 1; r <= N; r++) {
+		for(int t=1;t<=T;t++) {
+			N = sc.nextInt();
+			L = sc.nextInt();
+			score_arr = new int[N];
+			cal_arr = new int[N];
+			for(int r=0;r<N;r++) {
 				Ti = sc.nextInt();
-				score_arr[r] = Ti; // 점수
+				score_arr[r]=Ti;
 				Ki = sc.nextInt();
-				cal_arr[r] = Ki; // 칼로리
+				cal_arr[r]=Ki;
 			}
-
-			// 제한 칼로리만큼의 배열만들어서
-			int[][] dp = new int[N + 1][L + 1];
-
-			// 현재칼로리가 해당위치의 칼로리기준을 넘으면 이전행 같은열에 있는 값으로 해당자리 갱신 (이전 재료것이 최고여)
-			// 아니라면 이전행 이전열에 현재가치를 더한값과 이전행 같은열의 값을 비교
-			for (int i = 1; i <= N; i++) {
-				for (int j = 1; j <= L; j++) {
-					if (cal_arr[i] > j) {
-						dp[i][j] = dp[i - 1][j];
-					} else {
-						dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - cal_arr[i]] + score_arr[i]);
+			check = new boolean[N];
+			max=0;
+			combi(0);
+			System.out.println("#"+t+" "+max);
+		}
+	}
+	
+	public static void combi(int num) {	
+		if(num==N) {
+			cal_sum=0;
+			score_sum=0;
+			for(int i=0;i<N;i++) {
+				if(check[i]) {
+					cal_sum+=cal_arr[i];
+					score_sum+=score_arr[i];
+					if(cal_sum<=L) {
+						max=Math.max(score_sum, max);
 					}
 				}
 			}
-
-			System.out.println("#" + t + " " + dp[N][L]);
+			return;
 		}
-		sc.close();
+		check[num]=true;
+		combi(num+1);
+		check[num]=false;
+		combi(num+1);
 	}
 }
