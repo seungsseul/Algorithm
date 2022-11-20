b1663d20bc8ad8e376cd006436351a40851c443c
seungwan97 <101645602+seungwan97@users.noreply.github.com>
2022-11-20 23:34:35 +0900

    [D1] Title: 평균값 구하기, Time: 130 ms, Memory: 27,056 KB -BaekjoonHub

---
+++ SWEA/D1/2071. 평균값 구하기/평균값 구하기.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int i=1;i<=T;i++) {
+			int[] arr = new int[10];
+			for(int j=0;j<10;j++) {
+				arr[j] = sc.nextInt();
+			}
+			for(int k=0; k<arr.length-1;k++) {
+				for(int j=0; j<arr.length-1;j++) {
+					if(arr[j]>=arr[j+1]) {
+						int temp = arr[j+1];
+						arr[j+1]=arr[j];
+						arr[j]=temp;
+					}
+				}
+			}
+			int sum=0;
+			for(int k=0; k<arr.length;k++) {
+				sum+=arr[k];
+			}
+			System.out.println("#"+i+" "+Math.round((float)sum/(arr.length)));
+		}
+		sc.close();
+	}
+}
