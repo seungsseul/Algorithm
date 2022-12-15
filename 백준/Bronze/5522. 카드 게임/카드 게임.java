cf1298aaaeb708ad7c8e40b72ffa41d8ab094b47
seungwan97 <101645602+seungwan97@users.noreply.github.com>
2022-12-15 23:52:36 +0900

    [Bronze V] Title: 카드 게임, Time: 80 ms, Memory: 11576 KB -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static int sum;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		sum=Integer.parseInt(br.readLine())+Integer.parseInt(br.readLine())+Integer.parseInt(br.readLine())+Integer.parseInt(br.readLine())+Integer.parseInt(br.readLine());
+		bw.write(String.valueOf(sum));
+		bw.flush();bw.close();br.close();
+	}
+}
