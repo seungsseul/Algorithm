e7ff4298082e2094a8bdadf5eeaff00aa658693c
seungwan97 <101645602+seungwan97@users.noreply.github.com>
2022-12-24 23:54:55 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 700 ms, Memory: 241396 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+	static int T;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		T = Integer.parseInt(br.readLine());
+		for(int t=1;t<=T;t++) {
+			st = new StringTokenizer(br.readLine());
+			sb.append(Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken())).append("\n");
+		}
+		bw.write(sb.toString());
+		bw.flush();bw.close();br.close();
+	}
+}
