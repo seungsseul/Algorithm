0c3a75fcdcb72fe94ba6c18a0cc2b7966d0a1562
seungwan97 <101645602+seungwan97@users.noreply.github.com>
2023-02-17 16:12:05 +0900

    [Bronze V] Title: 구구단, Time: 76 ms, Memory: 11620 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringBuilder sb = new StringBuilder();
+	static int N;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		N = Integer.parseInt(br.readLine());
+		for(int i=1;i<=9;i++) {
+			sb.append(N).append(" ").append("*").append(" ").append(i).append(" ").append("=").append(" ").append(N*i).append("\n");
+		}
+		bw.write(sb.toString());
+		bw.close();br.close();
+	}
+}
