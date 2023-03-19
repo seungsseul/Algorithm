cbbcd5447989b4dc366e89803a225083c5d4d9be
seungwan97 <101645602+seungwan97@users.noreply.github.com>
2023-03-19 23:57:52 +0900

    Update 완주하지 못한 선수.java

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,14 +3,17 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
+	//정렬하고
         Arrays.sort(participant);
         Arrays.sort(completion);
+	//같은 인덱스의 값을 각각 비교했을때 다른 값이라면 해당 이름이 완주하지 못한 선수
         for(int i=0;i<completion.length;i++) {
             if(!participant[i].equals(completion[i])) {
                 answer=participant[i];
                 break;
             }
         }
+	//만약 걸린사람이 없다면 배열의 마지막 이름이 완주하지 못한 선수
         if(answer.equals("")) {
 	        answer=participant[participant.length-1];
 	    }
