da23f3af53d3460604b967add3adf0ebb45ac537
seungwan97 <101645602+seungwan97@users.noreply.github.com>
2023-03-29 20:45:40 +0900

    Update 숫자의 표현.java

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,6 +1,8 @@
 class Solution {
     public int solution(int n) {
         int ans = 0;
+	    	//완탐
+	    	//1부터 차례대로 1씩 더해진 수(plus)를 tmp에 더해가면서 만약 n보다 크면 다음수, n과 같으면 ans++해주고 다음수 비교 
 		for(int i=1;i<=n;i++) {	
 			int tmp=i;
 			int plus=tmp+1;
