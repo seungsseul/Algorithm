7c0d8f5180c97f2603114e583ffb1f1db559b1c9
seungwan97 <101645602+seungwan97@users.noreply.github.com>
2023-03-30 19:47:59 +0900

    Update 서울에서 김서방 찾기.java

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,6 +1,7 @@
 class Solution {
     public String solution(String[] seoul) {
         String answer = "김서방은 ";
+        //배열에서 Kim인 인덱스 찾으면 그게 답
         for(int i=0;i<seoul.length;i++) {
             if(seoul[i].equals("Kim")) {
                 answer+=i;
