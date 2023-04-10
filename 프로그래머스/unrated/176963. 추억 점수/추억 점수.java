9e3d225d22f266ccfb30839d5896c541b02e54ac
seungwan97 <101645602+seungwan97@users.noreply.github.com>
2023-04-10 17:57:09 +0900

    Update 추억 점수.java

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
@@ -5,7 +5,7 @@ class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
         List<Integer> list = new ArrayList<>();
 		int sum=0;
-
+		//완전탐색
 		for(int i=0;i<photo.length;i++) {
 			sum=0;
 			for(int j=0;j<photo[i].length;j++) {
@@ -23,6 +23,7 @@ class Solution {
 		}
 
 		int[] answer = new int[list.size()];
+	    	//리스트 배열로 바꿔주고 출력
 		for(int i=0;i<list.size();i++) {
 			answer[i]=list.get(i);
 		}
