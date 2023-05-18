a4b803601c48fabf1b9ec842be25529e32ed7c44
Seungwan Kim <101645602+seungwan97@users.noreply.github.com>
2023-05-18 17:11:44 +0900

    [unrated] Title: 뒤에서 5등까지, Time: 0.50 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+class Solution {
+    public int[] solution(int[] num_list) {
+        Arrays.sort(num_list);
+        int[] answer = {num_list[0], num_list[1],num_list[2],num_list[3],num_list[4]};
+        return answer;
+    }
+}
