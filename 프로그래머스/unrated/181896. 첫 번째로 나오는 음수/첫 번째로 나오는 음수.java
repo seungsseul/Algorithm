d93cfe88d468dd3980bf98784b12d18c0205adda
Seungwan Kim <101645602+seungwan97@users.noreply.github.com>
2023-07-11 23:57:29 +0900

    [unrated] Title: 첫 번째로 나오는 음수, Time: 0.04 ms, Memory: 82 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        for(int i=0;i<num_list.length;i++) {
+            if(num_list[i]<0) {
+                break;
+            }
+            answer++;
+        }
+        if(answer==num_list.length) {
+            answer=-1;
+        }
+        return answer;
+    }
+}
