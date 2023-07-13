747217e1f6d232735187c5112b106b6531f0da82
Seungwan Kim <101645602+seungwan97@users.noreply.github.com>
2023-07-14 00:09:08 +0900

    [level 0] Title: 숫자 찾기, Time: 0.04 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int num, int k) {
+        int answer = -1;
+        String str = String.valueOf(num);
+        for(int i=0;i<str.length();i++) {
+            if(Character.getNumericValue(str.charAt(i))==k) {
+                answer=i+1;
+                break;
+            }
+        }
+        return answer;
+    }
+}
