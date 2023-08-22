2cb7cd763d223c2ad1dc256b94f1faccd8f785bc
Seungwan Kim <101645602+seungwan97@users.noreply.github.com>
2023-08-23 00:11:32 +0900

    [unrated] Title: n개 간격의 원소들, Time: 0.04 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public List<Integer> solution(int[] num_list, int n) {
+        List<Integer> answer = new ArrayList<>();
+        for(int i=0;i<num_list.length;i+=n) {
+            answer.add(num_list[i]);
+        }
+        return answer;
+    }
+}
