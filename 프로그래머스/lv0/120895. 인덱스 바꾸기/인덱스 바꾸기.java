e0da860bbb9eae8d5087226a4349f007d501b254
Seungwan Kim <101645602+seungwan97@users.noreply.github.com>
2023-09-22 08:55:18 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 17.36 ms, Memory: 84 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String tmp = my_string.substring(0,num1)+String.valueOf(my_string.charAt(num2))+my_string.substring(num1+1,num2)+String.valueOf(my_string.charAt(num1))+my_string.substring(num2+1);
+        my_string=tmp;
+        return my_string;
+    }
+}
