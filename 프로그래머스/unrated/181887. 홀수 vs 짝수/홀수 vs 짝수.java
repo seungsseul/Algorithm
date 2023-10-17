bedd3456be6188e6964194a81723a04a4e2ea53c
Seungwan Kim <101645602+seungwan97@users.noreply.github.com>
2023-10-17 23:49:25 +0900

    [level unrated] Title: 홀수 vs 짝수, Time: 0.04 ms, Memory: 82 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int hol = 0; int jjak=0;
+        for(int i=0;i<num_list.length;i++) {
+            int a = (i%2==0) ? (hol += num_list[i]) : (jjak += num_list[i]);
+        }
+        return Math.max(hol,jjak);
+    }
+}
