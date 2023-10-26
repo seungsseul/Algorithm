15e49916db6546566c4eaf57accf8aa6df8571f1
Seungwan Kim <101645602+seungwan97@users.noreply.github.com>
2023-10-27 06:40:36 +0900

    [level unrated] Title: 할 일 목록, Time: 0.03 ms, Memory: 71 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,13 @@
+import java.util.List;
+import java.util.ArrayList;
+class Solution {
+    public List<String> solution(String[] todo_list, boolean[] finished) {
+        List<String> answer = new ArrayList<>();
+        for(int i=0;i<todo_list.length;i++) {
+            if(!finished[i]) {
+                answer.add(todo_list[i]);
+            }
+        }
+        return answer;
+    }
+}
