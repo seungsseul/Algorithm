846e3531e6db761598b8a1e70ca26e8beeacca31
Seungwan Kim <101645602+seungwan97@users.noreply.github.com>
2023-12-04 03:38:35 +0900

    [Bronze V] Title: 時間 (Hour), Time: 76 ms, Memory: 11548 KB -BaekjoonHub

---
+++ 백준/Bronze/27327. 時間 （Hour）/時間 （Hour）.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int hour = Integer.parseInt(br.readLine())*24;
+		bw.write(String.valueOf(hour));bw.flush();bw.close();br.close();
+	}
+}
