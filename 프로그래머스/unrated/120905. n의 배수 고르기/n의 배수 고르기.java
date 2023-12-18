e5901b388c4c4792eaf967b4822933b674d0f7e5
Seungwan Kim <101645602+seungwan97@users.noreply.github.com>
2023-12-18 23:51:18 +0900

    [level unrated] Title: n의 배수 고르기, Time: 0.03 ms, Memory: 97.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+class Solution {
+    public ArrayList<Integer> solution(int n, int[] numlist) {
+        ArrayList<Integer> answer = new ArrayList<Integer>();
+        for(int num : numlist) {
+            if(num%n==0) {
+                answer.add(num);
+            }
+        }
+        return answer;
+    }
+}
