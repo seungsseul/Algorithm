464cce7402a17e6592a56c6383e7dc2242e9951f
Seungwan Kim <101645602+seungwan97@users.noreply.github.com>
2023-12-24 00:02:11 +0900

    [Bronze III] Title: 세 수, Time: 76 ms, Memory: 11664 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	static int[] arr;
+	public static void main(String[] args) throws IOException {
+		st = new StringTokenizer(br.readLine());
+		arr = new int[3];
+		arr[0] = Integer.parseInt(st.nextToken());
+		arr[1] = Integer.parseInt(st.nextToken());
+		arr[2] = Integer.parseInt(st.nextToken());
+		Arrays.sort(arr);
+		bw.write(String.valueOf(arr[1]));bw.flush();bw.close();br.close();
+	}
+}
