12cacd3f88edcfaba51b2fefdf553f6ee9757121
Seungwan Kim <101645602+seungwan97@users.noreply.github.com>
2024-01-17 00:30:41 +0900

    [Silver II] Title: 섬의 개수, Time: 176 ms, Memory: 16372 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -1,59 +1,78 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-	
-	
+	static class Node {
+		int r;
+		int c;
+		int cnt;
+		public Node(int r, int c) {
+			this.r=r;
+			this.c=c;
+		}
+	}
+	static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+	static int w, h, cnt;
 	static int[][] map;
-    static int w, h;
-    static int[] dr = { -1, -1, 0, 1, 1, 1, 0, -1};
-    static int[] dc = { 0, 1, 1, 1, 0, -1, -1, -1};
-    
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        while(true) {
-            w = sc.nextInt();
-            h = sc.nextInt();
-            if(w == 0 && h == 0)
-                break;
-            map = new int[h][w];
-            for(int i = 0; i < h; i++) {
-                for(int j = 0; j < w; j++)
-                    map[i][j] = sc.nextInt();
-            }
-            //섬 탐색하면서 1나오면 연결된 육지 삭제 작업 시작
-            int cnt = 0;
-            for(int i = 0; i < h; i++) {
-                for(int j = 0; j < w; j++) {
-                    if( map[i][j] == 1 ) {
-                    	//섬하나 발견했으니 일단 그 면적이 어떻게 되던 카운트
-                        cnt++;
-                        //육지 삭제 작업 시작
-                        dfs(i, j);
-                    }
-                }
-            }
-            System.out.println(cnt);
-        }
-        sc.close();
-    }
-    
-    
-    
-    
-    static void dfs(int r, int c) {
-        //현재 육지를 삭제
-        map[r][c] = 0;
-        for(int d = 0; d < 8; d++) {
-            int nr = r + dr[d];
-            int nc = c + dc[d];
-            if( nr < 0 || nc < 0 || nr >= h || nc >= w ) {            	
-            	continue;
-            }
-            //바다면 continue, 아니면 연결된 육지 0으로 삭제
-            if( map[nr][nc] == 0 ) {            	
-            	continue;
-            }
-            dfs(nr, nc);
-        }
-    }
-}
+	static boolean[][] visited;
+	static int[] dr = {-1,1,0,0,-1,1,-1,1};
+	static int[] dc = {0,0,-1,1,-1,-1,1,1};
+	static Queue<Node> q;
+	public static void main(String[] args) throws IOException {
+		while(true) {
+			st = new StringTokenizer(br.readLine());
+			w = Integer.parseInt(st.nextToken());
+			h = Integer.parseInt(st.nextToken());
+			if(w==0 && h==0) {
+				break;
+			}
+			map = new int[h][w];
+			visited = new boolean[h][w];
+			for(int i=0;i<h;i++) {
+				st = new StringTokenizer(br.readLine());
+				for(int j=0;j<w;j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			cnt=0;
+			for(int i=0;i<h;i++) {
+				for(int j=0;j<w;j++) {
+					if(map[i][j]==1 && !visited[i][j]) {
+						bfs(i,j);
+						cnt++;
+					}
+				}
+			}
+			sb.append(cnt).append("\n");
+		}
+		bw.write(sb.toString());bw.close();br.close();
+	}
+	static void bfs(int i, int j) {
+		q = new LinkedList<Node>();
+		q.offer(new Node(i,j));
+		visited[i][j]=true;
+		while(!q.isEmpty()) {
+			Node n = q.poll();
+			for(int d=0;d<8;d++) {
+				int nr = n.r+dr[d];
+				int nc = n.c+dc[d];
+				if(nr>=0 && nc>=0 && nr<h && nc<w) {
+					if(visited[nr][nc] || map[nr][nc]==0) {
+						continue;
+					}
+					visited[nr][nc]=true;
+					q.offer(new Node(nr, nc));
+				}
+			}
+		}
+	}
+ }
