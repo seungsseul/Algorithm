d9b767a8494995b2f3fba9f20c7fa61042006033
Seungwan Kim <101645602+seungwan97@users.noreply.github.com>
2024-02-15 07:15:52 +0900

    [Bronze IV] Title: 햄버거 만들기, Time: 120 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze IV/25628. 햄버거 만들기/햄버거 만들기.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	static int A, B, ans;
+	public static void main(String[] args) throws IOException {
+		st = new StringTokenizer(br.readLine());
+		A = Integer.parseInt(st.nextToken());
+		B = Integer.parseInt(st.nextToken());
+		ans = 0;
+		while(A>1 && B>0) {
+			ans++;
+			A-=2;
+			B--;
+		}
+		bw.write(String.valueOf(ans));bw.flush();bw.close();br.close();
+	}
+}
