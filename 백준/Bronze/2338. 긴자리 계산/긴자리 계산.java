9ebc39a435cbd9ae8cf159469d83aecf5f115e02
Seungwan Kim <101645602+seungwan97@users.noreply.github.com>
2024-03-06 16:57:35 +0900

    [Bronze V] Title: 긴자리 계산, Time: 136 ms, Memory: 15264 KB -BaekjoonHub

--- 백준/Bronze/2338. 긴자리 계산/긴자리 계산.java
+++ 백준/Bronze/2338. 긴자리 계산/긴자리 계산.java
@@ -1,15 +1,19 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.math.BigInteger;
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        BigInteger first = in.nextBigInteger();
-        BigInteger second = in.nextBigInteger();
-
-        System.out.println(first.add(second));
-        System.out.println(first.subtract(second));
-        System.out.print(first.multiply(second));
-
-    }
+	static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static BigInteger A, B;
+	static StringBuilder sb = new StringBuilder();	
+	public static void main(String[] args) throws IOException {
+		A = new BigInteger(br.readLine());
+		B = new BigInteger(br.readLine());
+		sb.append(A.add(B)).append("\n").append(A.subtract(B)).append("\n").append(A.multiply(B));
+		bw.write(sb.toString());bw.close();br.close();
+	}
 }
