750603afcd564a0935c474c683104ded865dce63
Seungwan Kim <101645602+seungwan97@users.noreply.github.com>
2024-03-11 19:13:33 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 644 ms, Memory: 298156 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -10,22 +10,23 @@ public class Main {
 		
 		int N = sc.nextInt();
 		int K = sc.nextInt();
-		sc.close();
 		
 		for(int i=1; i<=N; i++)
 			queue.add(i);
 		
 		while(queue.size() > 1) {
+			//for문을 순회하면서 K번째가 나오기전까지 삭제와 삽입 반복
 			for(int i=1; i<K; i++) {
 				int value = queue.poll();
 				queue.offer(value);
 			}
-			
+			//K번째 수를 빼주면서 sb에 추가
 			sb.append(queue.poll()).append(", ");
 		}
-		
+		//queue에 요소가 1개남았을때 이를 마저빼주고 괄호닫긴
 		sb.append(queue.poll()).append('>');
 		
 		System.out.println(sb);
+		sc.close();
 	}
 }
