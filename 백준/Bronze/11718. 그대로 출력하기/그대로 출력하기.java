8c08b167b245b2a29066cafd84a9a1440b2dc511
Seungwan Kim <101645602+seungwan97@users.noreply.github.com>
2024-03-13 16:19:07 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 124 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -3,19 +3,19 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 	static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
 	static StringBuilder sb = new StringBuilder();
+	static String Input;
 	public static void main(String[] args) throws IOException {
-		String Input="";
-		while((Input = br.readLine()) != null) {			
-			sb.append(Input).append("\n");
+		while((Input = br.readLine()) != null) {
+			st = new StringTokenizer(Input,"");
+			sb.append(st.nextToken()).append("\n");
 		}
-		bw.write(sb.toString());
-		bw.close();
-		br.close();
-		
+		bw.write(sb.toString());bw.close();br.close();
 	}
 }
