a83bca41b2e5fe56e1da16a3b87763a24a2a360a
Seungwan Kim <101645602+seungwan97@users.noreply.github.com>
2024-03-31 02:13:16 +0900

    [Bronze IV] Title: 욱 제, Time: 124 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/17356. 욱 제/욱 제.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	static int A, B;
+	static double M;
+	public static void main(String[] args) throws IOException {
+		st = new StringTokenizer(br.readLine());
+		A = Integer.parseInt(st.nextToken());
+		B = Integer.parseInt(st.nextToken());
+		M = (B-A)/400.0;
+		bw.write(String.valueOf(1/(1+Math.pow(10, M))));bw.flush();
+		bw.close();br.close();
+	}
+}
