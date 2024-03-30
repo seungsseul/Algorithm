220bf7af3d7b5034e1c2cb44c7d4a4de3e265434
Seungwan Kim <101645602+seungwan97@users.noreply.github.com>
2024-03-30 11:45:02 +0900

    [Bronze III] Title: 부호, Time: 744 ms, Memory: 127632 KB -BaekjoonHub

--- 백준/Bronze/1247. 부호/부호.java
+++ 백준/Bronze/1247. 부호/부호.java
@@ -1,27 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.math.BigInteger;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		StringBuilder sb = new StringBuilder();
-		for(int t=1;t<=3;t++) {
-			int N = sc.nextInt();
-			BigInteger sum = new BigInteger("0");
-			for(int i=0;i<N;i++) {
-				sum=sum.add(sc.nextBigInteger());
-			}
-			if(sum.compareTo(new BigInteger("0"))==0) {
-				sb.append(0).append("\n");
-			}
-			else if(sum.compareTo(new BigInteger("0"))==1) {
-				sb.append("+").append("\n");
-			}
-			else if(sum.compareTo(new BigInteger("0"))==-1) {
-				sb.append("-").append("\n");
-			}
+	static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+	static long N;
+	static BigInteger S;
+	public static void main(String[] args) throws IOException {
+		for(int i=0;i<3;i++) {
+			testSet();
 		}
-		System.out.println(sb);
-		sc.close();
+		bw.write(sb.toString());bw.close();br.close();
+	}
+	static void testSet() throws IOException {
+		N = Long.parseLong(br.readLine());
+		S = new BigInteger("0");
+		for(int i=0;i<N;i++) {
+			S = S.add(new BigInteger(br.readLine()));
+		}
+		sb.append(S.compareTo(BigInteger.ZERO)==0 ? "0" : (S.compareTo(BigInteger.ZERO)==1 ? "+" : "-")).append("\n");
 	}
 }
