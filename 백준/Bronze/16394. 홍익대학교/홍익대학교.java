b6e2aeadfa9dd78a5e63deedbe04f2ada5763fbc
Seungwan Kim <101645602+seungwan97@users.noreply.github.com>
2024-04-09 23:50:25 +0900

    [Bronze V] Title: 홍익대학교, Time: 124 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Bronze/16394. 홍익대학교/홍익대학교.java
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -1,9 +1,13 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		System.out.println(N-1946);
+	static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws IOException {
+		bw.write(String.valueOf(Integer.parseInt(br.readLine())-1946));bw.flush();bw.close();br.close();
 	}
 }
