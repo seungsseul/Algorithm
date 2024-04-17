207201fa1edb47df5af20bb4235431a8a8cde5fc
Seungwan Kim <101645602+seungwan97@users.noreply.github.com>
2024-04-17 18:13:38 +0900

    [Silver III] Title: 킹, Time: 152 ms, Memory: 16272 KB -BaekjoonHub

--- 백준/Silver/1063. 킹/킹.java
+++ 백준/Silver/1063. 킹/킹.java
@@ -10,102 +10,56 @@ public class Main {
 	static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringTokenizer st;
 	static StringBuilder sb = new StringBuilder();
-	static int[][] arr;
-	static String k, s, od;
-	static int kingR, kingC, stoneR, stoneC, N;
-	static int[] dr = {0, 0, 1, -1, -1, -1, 1, 1};
-	static int[] dc = {1, -1, 0, 0, 1, -1, 1, -1};
-	static String[] order = {"R", "L", "B", "T", "RT", "LT", "RB", "LB"};
-	static class King {
-		int kr;
-		int kc;
-		public King(int kr, int kc) {
-			this.kr = kr;
-			this.kc = kc;
-		}
-		
-	}
-	static class Stone {
-		int sr;
-		int sc;
-		public Stone(int sr, int sc) {
-			super();
-			this.sr = sr;
-			this.sc = sc;
-		}
-	}
+	static int[][] map;
+	static String[] king_tmp, stone_tmp; 
+	static int[] king, stone;
+	static int N;
+	static String Input;
+	static int[] dr = {0,0,-1,1,-1,-1,1,1};
+	static int[] dc = {-1,1,0,0,-1,1,-1,1};
+	static String[] order = {"L", "R", "T", "B", "LT", "RT", "LB", "RB"};
 	public static void main(String[] args) throws IOException {
+		map = new int[8][8];
+		king = new int[2]; stone = new int[2];
 		st = new StringTokenizer(br.readLine());
-		k = st.nextToken(); //1
-		kingR = 8-Character.getNumericValue(k.charAt(1));
-		kingC = ((int)(k.charAt(0))-64)-1;
-		s = st.nextToken(); //2
-		stoneR = 8-Character.getNumericValue(s.charAt(1));
-		stoneC = ((int)(s.charAt(0))-64)-1;
+		king_tmp = st.nextToken().split("");
+		king[1]=8-Integer.parseInt(king_tmp[1]);
+		king[0]=king_tmp[0].charAt(0)-65;
+		stone_tmp = st.nextToken().split("");
+		stone[1]=8-Integer.parseInt(stone_tmp[1]);
+		stone[0]=stone_tmp[0].charAt(0)-65;
+		map[king[1]][king[0]]=1;
+		map[stone[1]][stone[0]]=2;
 		N = Integer.parseInt(st.nextToken());
-		arr = new int[8][8];
-		arr[kingR][kingC]=1;
-		arr[stoneR][stoneC]=2;
-		move(new King(kingR, kingC), new Stone(stoneR, stoneC), 1);
-		loop:
-		for(int i=0;i<8;i++) {
-			for(int j=0;j<8;j++) {
-				if(arr[i][j]==1) {
-					k = String.valueOf((char)(j+65))+String.valueOf(8-i);
-					sb.append(k).append("\n");
-					break loop;
-				}
-			}
-		}
-		loop:
-		for(int i=0;i<8;i++) {
-			for(int j=0;j<8;j++) {
-				if(arr[i][j]==2) {
-					s = String.valueOf((char)(j+65))+String.valueOf(8-i);
-					sb.append(s);
-					break loop;
-				}
-			}
-		}
-		bw.write(sb.toString());
-		bw.flush();bw.close();br.close();
-	}
-	static void move(King king, Stone stone, int cnt) throws IOException {
-		if(cnt==N+1) {
-			return;
-		}
-		od = br.readLine();
-		for(int d=0;d<8;d++) {
-			if(od.equals(order[d])) {					
-				int kr = king.kr+dr[d];
-				int kc = king.kc+dc[d];
-				if(kr>=0 && kc>=0 && kr<8 && kc<8) {
-					arr[kr][kc]+=arr[king.kr][king.kc];
-					arr[king.kr][king.kc]=0;
-					if(arr[kr][kc]==3) {
-						if(kr+dr[d]>=0 && kc+dc[d]>=0 && kr+dr[d]<8 && kc+dc[d]<8) {
-							arr[kr][kc]-=2;
-							arr[kr+dr[d]][kc+dc[d]]+=2;
-							move(new King(kr, kc), new Stone(kr+dr[d], kc+dc[d]), cnt+1);
+		for(int t=1;t<=N;t++) {
+			Input = br.readLine();
+			for(int i=0;i<8;i++) {
+				if(Input.equals(order[i])) {
+					int nr = dr[i]+king[1];
+					int nc = dc[i]+king[0];
+					if(nr>=0 && nr<8 && nc>=0 && nc<8) {						
+						if(map[nr][nc]==2) {
+							int sr = dr[i]+nr;
+							int sc = dc[i]+nc;
+							if(sr>=0 && sr<8 && sc>=0 && sc<8) {
+								map[nr][nc]=1;
+								map[sr][sc]=2;
+								map[king[1]][king[0]]=0;
+								king[1]=nr; king[0]=nc;
+								stone[1]=sr; stone[0]=sc;
+							}
 						}
 						else {
-							arr[king.kr][king.kc]++;
-							arr[kr][kc]--;
-							if(cnt==N) {
-								return;
-							}
-							move(new King(king.kr, king.kc), new Stone(stone.sr, stone.sc), cnt+1);
+							map[king[1]][king[0]]=0;
+							map[nr][nc]=1;
+							king[1]=nr; king[0]=nc;
 						}
 					}
-					else {						
-						move(new King(kr, kc), new Stone(stone.sr, stone.sc), cnt+1);
-					}
 				}
-				else {
-					move(new King(king.kr, king.kc), new Stone(stone.sr, stone.sc), cnt+1);
-				}
-				break;
 			}
 		}
+		sb.append((char)(king[0]+65)+""+Math.abs(king[1]-8)).append("\n");
+		sb.append((char)(stone[0]+65)+""+Math.abs(stone[1]-8));
+		bw.write(sb.toString());bw.close();br.close();
 	}
 }
