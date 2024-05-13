796c766e0c354a70d544a953979a16c99934e802
Seungwan Kim <101645602+seungwan97@users.noreply.github.com>
2024-05-13 23:28:25 +0900

    [Bronze V] Title: A-B, Time: 128 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		st = new StringTokenizer(br.readLine());
+		bw.write(String.valueOf(Integer.parseInt(st.nextToken())-Integer.parseInt(st.nextToken())));
+		bw.flush();bw.close();br.close();
+	}
+}
