e1a1cca9861eb56fee67a60f7bb2d7942b1f4a46
Seungwan Kim <101645602+seungwan97@users.noreply.github.com>
2024-05-18 13:02:31 +0900

    [Bronze III] Title: 암호 만들기, Time: 104 ms, Memory: 14184 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21553. 암호 만들기/암호 만들기.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static String A, P;
+	public static void main(String[] args) throws IOException {
+		A = br.readLine();
+		P = br.readLine();
+		bw.write(P);bw.flush();bw.close();br.close();
+	}
+}
