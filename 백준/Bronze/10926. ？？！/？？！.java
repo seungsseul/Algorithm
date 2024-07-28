ee06d52c3040e1172d74a70dcf09cc6592568a40
Seungwan Kim <101645602+seungsseul@users.noreply.github.com>
2024-07-28 20:54:44 +0900

    [Bronze V] Title: ??!, Time: 128 ms, Memory: 14352 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws IOException {
+		bw.write(br.readLine()+"??!");bw.flush();bw.close();br.close();
+	}
+}
